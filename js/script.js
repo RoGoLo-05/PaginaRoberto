fec68d3ee4d29e665857d3807b593395e711169b
RoGoLo-05 <robertogomezlopez2005@gmail.com>
2024-05-28 20:24:09 +0200

    script en los botones y mas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//En verdad esto lo podría haber puesto en el html dentro del onclick sin que fuese una función, pero bueno ya que he creado este archivo pal javascript pues lo pongo aquí
+function confirmarDescarga() {
+    return confirm('¿Estás seguro de querer descargar el currículum?'); 
+}
+
+function confirmarGithub() {
+    return confirm('¿Estás seguro de querer ir a Github?'); 
+}
